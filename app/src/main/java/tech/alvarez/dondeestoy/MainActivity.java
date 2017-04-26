c1a84a246e458218fd78db4bd14671079eacd2a6
Daniel Alvarez <a3dany@gmail.com>
2017-04-26 15:08:19 -0400

    Start point

--- app/src/main/java/tech/alvarez/dondeestoy/MainActivity.java
+++ app/src/main/java/tech/alvarez/dondeestoy/MainActivity.java
@@ -31,8 +31,9 @@ import tech.alvarez.dondeestoy.model.Lugar;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private GoogleApiClient googleApiClient;
     private ImageView fotoImageView;
+
+    // TODO: El GoogleApiClient
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +42,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         fotoImageView = (ImageView) findViewById(R.id.fotoImageView);
 
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
+        // TODO: Construcción del GoogleApiClient
     }
 
     @Override
@@ -88,15 +84,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     protected void onStart() {
         super.onStart();
-        googleApiClient.connect();
+        // TODO: Conectamos el GoogleApiClient
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (googleApiClient.isConnected()) {
-            googleApiClient.disconnect();
-        }
+        // TODO: Desconectamos el GoogleApiClient
     }
 
 
@@ -109,74 +103,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             return;
         }
 
-        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi.getCurrentPlace(googleApiClient, null);
-        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
-            @Override
-            public void onResult(@NonNull PlaceLikelihoodBuffer placeLikelihoods) {
-
-                if (placeLikelihoods.getStatus().isSuccess()) {
-
-                    ArrayList<Lugar> datos = new ArrayList<Lugar>();
-
-                    for (PlaceLikelihood placeLikelihood : placeLikelihoods) {
-                        Place place = placeLikelihood.getPlace();
-
-                        String id = place.getId();
-                        String nombre = place.getName().toString();
-                        String direccion = place.getAddress().toString();
-                        double latitud = place.getLatLng().latitude;
-                        double longitud = place.getLatLng().longitude;
-                        float probabilidad = placeLikelihood.getLikelihood();
-                        float calificacion = place.getRating();
-
-                        datos.add(new Lugar(id, nombre, direccion, latitud, longitud, probabilidad, calificacion));
-                    }
-                    placeLikelihoods.release();
-
-                    if (datos.size() > 0) { // si hay al menos un lugar obtenemos una foto del primero
-                        obtenerFoto(datos.get(0).getId());
-                    }
-
-                    LugaresAdapter lugaresAdapter = new LugaresAdapter(MainActivity.this, datos);
-                    ListView lugareslistView = (ListView) findViewById(R.id.listView);
-                    lugareslistView.setAdapter(lugaresAdapter);
-
-                } else {
-                    Toast.makeText(MainActivity.this, "Error obtener lugares: " + placeLikelihoods.getStatus().getStatusMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        // TODO: Método para obtener lugares
     }
 
 
     private void obtenerFoto(String idLugar) {
 
-        Places.GeoDataApi.getPlacePhotos(googleApiClient, idLugar).setResultCallback(new ResultCallback<PlacePhotoMetadataResult>() {
-            @Override
-            public void onResult(@NonNull PlacePhotoMetadataResult photos) {
+        // TODO: Método para obtener una imagen del lugar enviado
 
-                if (photos.getStatus().isSuccess()) {
-                    PlacePhotoMetadataBuffer photoMetadataBuffer = photos.getPhotoMetadata();
-                    if (photoMetadataBuffer.getCount() > 0) {
-                        photoMetadataBuffer.get(0).getScaledPhoto(googleApiClient, fotoImageView.getWidth(), fotoImageView.getHeight())
-                                .setResultCallback(new ResultCallback<PlacePhotoResult>() {
-                                    @Override
-                                    public void onResult(@NonNull PlacePhotoResult placePhotoResult) {
-                                        if (placePhotoResult.getStatus().isSuccess()) {
-                                            fotoImageView.setImageBitmap(placePhotoResult.getBitmap());
-                                        } else {
-                                            Toast.makeText(MainActivity.this, "Error obtener foto: " + placePhotoResult.getStatus().getStatusMessage(), Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
-                                });
-                    } else {
-                        Toast.makeText(MainActivity.this, "El lugar no tiene fotos", Toast.LENGTH_SHORT).show();
-                    }
-                    photoMetadataBuffer.release();
-                } else {
-                    Toast.makeText(MainActivity.this, "Error encontrar fotos de lugar: " + photos.getStatus().getStatusMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
